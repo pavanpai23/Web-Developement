525bfe06177219dee1f6210242501b40805aaeca
pavanpai23 <pavanpai2006@gmail.com>
2025-08-20 20:36:37 +0530

    js

---
+++ js/55_var/script.js
@@ -0,0 +1,33 @@
+console.log("this is my 1st js")
+
+// var is not used
+
+let a = 20;
+let b = 3;
+let c = "pav";
+// let 55a="29" not allowed
+
+// console.log(a + b)
+// console.log(typeof a , typeof b , typeof c)
+
+console.log(a)
+const a1 = 9;
+// a1=a1+1;  not allowed if we declear constant as first
+
+
+let x = "pav";
+let y = 23;
+let z = 3.8;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "pav",
+    "job code": 239
+}
+
+console.log(o)
